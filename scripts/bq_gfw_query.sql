8a8a2dbfc311afa405ce3c546b70ce267dc3cff9
theamarks <amarks1@stanford.edu>
2023-06-21 18:23:20 -0700

    updated bq query

--- scripts/bq_gfw_query.sql
+++ scripts/bq_gfw_query.sql
@@ -8,4 +8,21 @@ SELECT  lat, lon, timestamp, hours, nnet_score
 
 FROM `world-fishing-827.pipe_chile_production_v20211126.research_positions` 
 WHERE 
-DATE(timestamp) = "2023-05-01"
+--DATE(timestamp) = "2023-05-01"
+  _PARTITIONTIME >= "2023-03-01 00:00:00"
+  AND _PARTITIONTIME < "2023-03-31 00:00:00"
+
+---
+SELECT
+  SUM(fishing_hours) AS total_fishing_hours,
+  geartype
+FROM
+  [global-fishing-watch:global_footprint_of_fisheries.fishing_effort]
+WHERE
+  _PARTITIONTIME >= "2016-01-01 00:00:00"
+  AND _PARTITIONTIME < "2017-01-01 00:00:00"
+  AND flag = 'NOR'
+GROUP BY
+  geartype
+ORDER BY
+  total_fishing_hours DESC
