d64436b8081ded6c1003a3183f3d2a747508338d
theamarks <amarks1@stanford.edu>
2023-09-12 14:45:12 -0700

    replaced examples with SQL query used to pull gfw data

--- scripts/gfw_chl_ais_easter_island.sql
+++ scripts/gfw_chl_ais_easter_island.sql
@@ -1,64 +1,63 @@
--- pacific rim shape
-pacific as (
-  select st_geogfromtext(geometry, make_valid => TRUE) as geometry
-  from `gfwanalysis.pacific.pacific_rim`
-),
--- GFW anchorages
-anchorage as (
-  select s2id, st_geogpoint(lon, lat) as loc
-  from `world-fishing-827.anchorages.named_anchorages_v20220511`
-),
--- anchorages in the Pacific rim
-anchorage_pacific as (
-    select s2id from pacific
-    cross join (select * from anchorage)
-    where st_within(loc, geometry)
+-- AIS fishing events within Chile Easter Island EEZ
+
+WITH
+easter_island as (
+  SELECT 
+    ST_GEOGFROMTEXT(string_field_0) as eez_geometry
+  FROM 
+    `sdss-scrt-marks.chile_geospatial.easter-island-eez-wtk`
+  LIMIT 1  -- Assuming there's only one multipolygon representing the entire Easter Island EEZ
 ),
 
-
-with
 fishing_ais as (
-  select
+  SELECT
     timestamp,
     extract(year from timestamp) as year,
     ssvid,
     lat,
     lon,
+    ST_GEOGPOINT(lon, lat) as point_geometry,
     hours,
     nnet_score,
-    night
-  from `world-fishing-827.gfw_research.pipe_v20201001_fishing`
-  where timestamp between '2021-01-01' and '2021-01-03'
-    and (lat > 10 and lon > 10)
+    night_loitering
+  FROM 
+    `world-fishing-827.gfw_research.pipe_v20201001_fishing`
+  WHERE _PARTITIONTIME -- table is partitioned by day - use this to limit query size
+    BETWEEN '2023-01-01' 
+    AND '2023-07-31'
+    -- AND lat > 10 AND lon > 10 -- use this to create a 'bounding box' around of area of interest need to insert > & < to create rectangular. 
 ),
-chile as (
-  -- EEZ shape file
+
+easter_fishing as(
+SELECT 
+  fishing_ais.* 
+FROM 
+  fishing_ais, easter_island
+WHERE 
+  ST_CONTAINS(eez_geometry, point_geometry) -- Returns TRUE if the polygon contains the point inside it.
 ),
-fishing_ais_chile as (
-  -- filter fishing_ais within chile
+
+ais_chile_info as (
+  select * 
+  from easter_fishing
+  left join (select * from `world-fishing-827.gfw_research.fishing_vessels_ssvid_v20230801`)
+  using (ssvid, year)
 ),
-fishing_ais_info as (
-  select * from fishing_ais_chile
-  left join (
-    select
-      ssvid,
-      year,
-      if(best_flag = 'UNK', null, best_flag) as flag,
-      best_vessel_class as gear_type
-      from `world-fishing-827.gfw_research.fishing_vessels_ssvid`
-  )
-  using(ssvid, year)
-),
-fishing_ais_fishing as (
-  -- only select AIS when fishing
-  select *
-  from fishing_ais_info
-  where nnet_score > 0.5
-    or (gear_type = 'squid_jigger' and night_loitering = 1)
+
+ais_chile_fishing as (
+select *,
+  CASE 
+    WHEN best_vessel_class = 'squid_jigger' 
+      AND night_loitering = 1 
+      THEN hours
+    WHEN nnet_score > 0.5 
+      THEN hours
+    ELSE 0
+  END as fishing_hours
+from ais_chile_info
 )
-select
-  flag,
-  gear_type,
-  sum(hours) as fishing_hours
-from fishing_ais_fishing
-group by flag, gear_type
+
+SELECT * 
+FROM ais_chile_fishing
+WHERE fishing_hours > 0 
+
